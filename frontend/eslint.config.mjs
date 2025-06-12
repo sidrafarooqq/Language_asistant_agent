6a6649dd805275685a5f3eefb8b4fbe338a7b31c
“sidra <fsidra167@gmail.com>
2025-06-12 21:39:02 +0500

    initial commit

--- frontend/eslint.config.mjs
+++ frontend/eslint.config.mjs
@@ -11,6 +11,13 @@ const compat = new FlatCompat({
 
 const eslintConfig = [
   ...compat.extends("next/core-web-vitals", "next/typescript"),
+
+  // 👇 Custom config object add karo yahan
+  {
+    rules: {
+      "@typescript-eslint/no-explicit-any": "off",
+    },
+  },
 ];
 
 export default eslintConfig;
